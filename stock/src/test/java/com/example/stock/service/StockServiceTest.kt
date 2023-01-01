73a8b2c1536da692a149e2b356c179696ec5da71
JungwooSim <cscd053@gmail.com>
2023-01-01 17:40:44 +0900

    add : 005 - 문제점

--- stock/src/test/java/com/example/stock/service/StockServiceTest.kt
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.kt
@@ -5,10 +5,12 @@ import com.example.stock.repository.StockRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
+import java.util.concurrent.*
 
 @SpringBootTest
 class StockServiceTest @Autowired constructor(
@@ -35,5 +37,29 @@ class StockServiceTest @Autowired constructor(
     val stock = stockRepository.findByIdOrNull(1L) ?: throw RuntimeException("stock null error")
 
     assertEquals(99, stock.quantity)
+  }
+
+  @DisplayName("동시에_100개의_요청")
+  @Test
+  fun request_100() {
+    val threadCount = 100
+    val executorService: ExecutorService = Executors.newFixedThreadPool(100)
+    val latch: CountDownLatch = CountDownLatch(threadCount) // 다른 스레드에서 수행중인 작업을 마무리할 때까지 기다리기 위해 사용
+
+    for (i in 0 until threadCount) {
+      executorService.submit{
+        try {
+          stockService.decrease(1L, 1L)
+        } finally {
+          latch.countDown()
+        }
+      }
+    }
+
+    latch.await()
+
+    val stock: Stock = stockRepository.findByIdOrNull(1L) ?: throw RuntimeException("stock null error")
+
+    assertEquals(0L, stock.quantity) // 100 - (1 * 100) = 0
   }
 }
