326291cdcab4698063eaacb6364ac6db8b5c9e22
JungwooSim <cscd053@gmail.com>
2023-01-11 23:09:57 +0900

    add : 11 - Named Lock 활용해보기

--- stock/src/main/java/com/example/stock/service/StockService.kt
+++ stock/src/main/java/com/example/stock/service/StockService.kt
@@ -4,12 +4,15 @@ import com.example.stock.domain.Stock
 import com.example.stock.repository.StockRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StockService(
   private val stockRepository: StockRepository
 ) {
 
+  @Transactional(propagation = Propagation.REQUIRED)
   @Synchronized
   fun decrease(id: Long, quantity: Long) {
     val stock: Stock = stockRepository.findByIdOrNull(id) ?: throw RuntimeException("stock null error")
